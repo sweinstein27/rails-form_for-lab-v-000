1eeb72f53375c7314113ce89bee6dadfee1eb9c6
Scott Weinstein <sweinstein27@gmail.com>
2018-09-06 17:16:50 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -10,9 +10,7 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.new
-    @school_class.title = params[:title]
-    @school_class.room_number = params[:room_number]
+    @school_class = SchoolClass.new(params.require(:school_class))
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
